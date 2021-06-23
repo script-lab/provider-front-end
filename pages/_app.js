3ad2982715e63db10233e90b97da7ed4fd167a7b
script-lab <transmit.work@gmail.com>
2021-06-23 22:10:21 +0900

    [add] Material-ui を使用するための設定を追加
    Material-ui を SSR に対応させるために設定を追加した。

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
 import '../styles/globals.css'
+import Head from 'next/head';
+import React, {useEffect} from 'react';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // material-ui の SSR 対策
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
+
+  return (
+    <>
+      <Head>
+        <title>next-js-demo</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
+
 
 export default MyApp
